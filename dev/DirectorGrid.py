32f0e698ecb5b4f828edba3bd0e3dc18196b2634
peteflorence <pete.florence@gmail.com>
2016-02-24 11:50:06 -0500

    adds grid visualization demo

---
+++ dev/DirectorGrid.py
@@ -0,0 +1,36 @@
+from ddapp import consoleapp
+from ddapp import objectmodel as om
+from ddapp import visualization as vis
+from ddapp import vtkAll as vtk
+from ddapp import applogic
+from ddapp import vtkNumpy as vnp
+
+import numpy as np
+
+
+app = consoleapp.ConsoleApp()
+view = app.createView(useGrid=False)
+
+cellSize = 0.5
+numberOfCells = 2
+
+grid = vtk.vtkGridSource()
+grid.SetScale(cellSize)
+grid.SetGridSize(numberOfCells)
+grid.SetSurfaceEnabled(True)
+grid.Update()
+grid = grid.GetOutput()
+
+
+pts = vnp.getNumpyFromVtk(grid, 'Points')
+heatMap = np.random.randn(len(pts))
+
+vnp.addNumpyToVtk(grid, heatMap, 'heat_map')
+
+gridObj = vis.showPolyData(grid, 'heat map', colorByName='heat_map', parent='scene')
+gridObj.setProperty('Surface Mode', 'Surface with edges')
+
+applogic.resetCamera()
+
+view.show()
+app.start()
