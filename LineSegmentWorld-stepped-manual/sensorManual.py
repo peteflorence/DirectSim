3e57c04d4abe8744ba26246cab78525c4a868519
peteflorence <pete.florence@gmail.com>
2016-04-26 11:35:36 -0400

    implemented manual raycasting, not yet tested

--- LineSegmentWorld-stepped-manual/sensorManual.py
+++ LineSegmentWorld-stepped-manual/sensorManual.py
@@ -38,30 +38,113 @@ class SensorObjManual(object):
 
         self.LineSegmentEndpoints[-1,:] = [firstX, firstY, firstZ]
 
+        self.numLineSegmentEndpoints = np.shape(self.LineSegmentEndpoints)[0]
         print self.LineSegmentEndpoints
+
+    def LineIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
+        if ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)) == 0:
+            print "These lines are parallel"
+            return
+        x1, y1, x2, y2, x3, y3, x4, y4 = CastPointsToDouble(x1, y1, x2, y2, x3, y3, x4, y4)
+        Px = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+        Py = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / ((x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4))
+        return [Px, Py]
+
+    def IntersectionIsOnSegments(x1, y1, x2, y2, x3, y3, x4, y4, Px, Py):
+        # Check if Px is in the right range for Line 1
+        if ((x1 > Px) and (x2 > Px)) or ((x1 < Px) and (x2 < Px)):
+            return False
+        
+        # Check if Px is in the right range for Line 2
+        if ((x3 > Px) and (x4 > Px)) or ((x3 < Px) and (x4 < Px)):
+            return False
+        
+        # Check if Py is in the right range for Line 1
+        if ((y1 > Py) and (y2 > Py)) or ((y1 < Py) and (y2 < Py)):
+            return False
+        
+        # Check if Py is in the right range for Line 2
+        if ((y3 > Py) and (y4 > Py)) or ((y3 < Py) and (y4 < Py)):
+            return False
+        
+        return True
+
+    def LineSegmentIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
+        Point = LineSegmentIntersection(x1, y1, x2, y2, x3, y3, x4, y4)
+        if Point is None:
+            print "parallel lines, no intersection"
+            return
             
-        #     firstX = firstRaycastLocations[i,0]
-        #     firstY = firstRaycastLocations[i,1]
+        if not IntersectionIsOnSegments(x1, y1, x2, y2, x3, y3, x4, y4, Point[0], Point[1]):
+            print "NOT on the line segments"
+            return
 
-        #     secondX = firstRaycastLocations[i+1,0]
-        #     secondY = firstRaycastLocations[i+1,1]
-            
-        #     firstEndpt = (firstX,firstY,0.0)
-        #     secondEndpt = (secondX,secondY,0.0)
+        return Point
 
-        #     d.addLine(firstEndpt, secondEndpt, radius=0.1)
 
-        # firstX = firstRaycastLocations[len(firstRaycastLocations)-1,0]
-        # firstY = firstRaycastLocations[len(firstRaycastLocations)-1,1]
+    def IntersectionDistance(x1, y1, x2, y2, x3, y3, x4, y4):
+        
+        # Line 1, the laser, is (x1, y1) , (x2, y2)
 
-        # secondX = -0.4
-        # secondY = 0.0
-            
-        # firstEndpt = (firstX,firstY,0.0)
-        # secondEndpt = (secondX,secondY,0.0)
+        # (x1, y1) is state of vehicle
+        # (x2, y2) is a ray projected forward to the maximum range
 
-        # d.addLine(firstEndpt, secondEndpt, radius=0.1)
+
+        # Line 2, the line segment in the world, is (x3, y3), (x4, y4)
+
+        # (x3, y3) is left side of line segment
+        # (x4, y4) is right side of line segment
+
+        Point = LineSegmentIntersection(x1, y1, x2, y2, x3, y3, x4, y4):
+
+        if Point is None:
+            return "NO INTERSECTION"
+
+        # return length of intersecting laser
+
+        Length = ((Point[0] - x1)**2 + (Point[1] - y1)**2)**(0.5)
+
+        return Length
+
+
+
+
 
     def raycastAllManual(self,frame):
         distances = np.zeros(self.numRays)
+
+        origin = np.array(frame.transform.GetPosition())
+        
+        for i in range(self.numRays):
+
+            # transform the ray and find the max range location
+
+            rayTransformed = np.array(frame.transform.TransformNormal(ray))
+            maxRangeLocation = origin + rayTransformed*self.rayLength
+
+
+            # initialize a vector of all of the intersections
+
+            laserWithEachWorldSegment = np.zeros((self.numLineSegmentEndpoints-1))
+            
+
+            # find intersections with this laser with each line segment in the world 
+
+            for j in range(self.numLineSegmentEndpoints-1):
+
+                 length = IntersectionDistance(origin[0], origin[1], maxRangeLocation[0], maxRangeLocation[1], self.LineSegmentEndpoints[j,0], self.LineSegmentEndpoints[j,1], self.LineSegmentEndpoints[j+1,0], self.LineSegmentEndpoints[j+1,1])
+                 if length is None:
+                    length = self.RayLength
+                 laserWithEachWorldSegment[j] = length
+
+
+            # choose the smallest distance
+
+            distances[i] = np.min(laserWithEachWorldSegment)
+
+
         return distances
+
+
+
+
